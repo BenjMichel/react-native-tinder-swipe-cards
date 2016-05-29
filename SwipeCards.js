a870a1334a61f6cbc14eab85569c40159d2278e0
Benjamin Michel <benjaminm@bamlab.fr>
2016-05-30 00:12:16 +0200

    Set background color as transparent

--- SwipeCards.js
+++ SwipeCards.js
@@ -197,7 +197,7 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'rgba(0,0,0,0)',
   },
   yup: {
     borderColor: 'green',
